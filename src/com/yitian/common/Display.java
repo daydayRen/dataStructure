5a8d4201e58f428321e037ec6b676b1bef118486
daydayRen <1209050193@qq.com>
2019-05-14 09:42:01 +0800

    公共的打印方法

--- src/com/yitian/common/Display.java
+++ src/com/yitian/common/Display.java
@@ -9,21 +9,14 @@ package com.yitian.common;
  */
 public class Display {
 
-    private int[] array;
+    //private static int[] array;
 
-    public Display(int[] array) {
-        this.array = array;
+    public static void  Display(int[] array) {
         for(int i=0;i<array.length;i++){
             System.out.print(array[i]+"\t");
         }
         System.out.println();
     }
-    /*//按顺序打印数组中的元素
-    public void display(){
-        for(int i=0;i<array.length;i++){
-            System.out.print(array[i]+"\t");
-        }
-        System.out.println();
-    }*/
+
 
 }
